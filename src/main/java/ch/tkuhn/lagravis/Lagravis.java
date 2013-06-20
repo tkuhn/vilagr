f1818ad6e7e1039ac5f715f42993948a821d6a29
Tobias Kuhn <kuhntobias@gmail.com>
2013-06-20 17:09:04 +0200

    Colorize graph; export PDF and PNG

--- src/main/java/ch/tkuhn/lagravis/Lagravis.java
+++ src/main/java/ch/tkuhn/lagravis/Lagravis.java
@@ -5,6 +5,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.gephi.data.attributes.api.AttributeColumn;
+import org.gephi.data.attributes.api.AttributeController;
+import org.gephi.data.attributes.api.AttributeModel;
+import org.gephi.data.attributes.api.AttributeTable;
+import org.gephi.data.attributes.api.AttributeType;
 import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.io.exporter.api.ExportController;
@@ -13,6 +18,9 @@ import org.gephi.io.importer.api.ImportController;
 import org.gephi.io.processor.plugin.DefaultProcessor;
 import org.gephi.layout.plugin.openord.OpenOrdLayoutBuilder;
 import org.gephi.layout.spi.Layout;
+import org.gephi.partition.api.Partition;
+import org.gephi.partition.api.PartitionController;
+import org.gephi.partition.plugin.NodeColorTransformer;
 import org.gephi.preview.api.PreviewController;
 import org.gephi.preview.api.PreviewModel;
 import org.gephi.preview.api.PreviewProperty;
@@ -25,6 +33,9 @@ public class Lagravis {
 
 	public static void main(String[] args) {
 		File inputFile = new File(args[0]);
+		String typeCol = "type";
+		if (args.length > 1) typeCol = args[1];
+
 		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
 		pc.newProject();
 		Workspace ws = pc.getCurrentWorkspace();
@@ -39,13 +50,27 @@ public class Lagravis {
 		GraphModel gm = Lookup.getDefault().lookup(GraphController.class).getModel();
 
 		PreviewModel model = Lookup.getDefault().lookup(PreviewController.class).getModel();
-		model.getProperties().putValue(PreviewProperty.EDGE_COLOR, new EdgeColor(Color.BLUE));
+		model.getProperties().putValue(PreviewProperty.EDGE_COLOR, new EdgeColor(Color.GRAY));
 		model.getProperties().putValue(PreviewProperty.EDGE_CURVED, false);
 		model.getProperties().putValue(PreviewProperty.EDGE_OPACITY, 2);
 		model.getProperties().putValue(PreviewProperty.ARROW_SIZE, 0);
 		model.getProperties().putValue(PreviewProperty.EDGE_THICKNESS, 1);
 		model.getProperties().putValue(PreviewProperty.NODE_BORDER_WIDTH, 0);
-		model.getProperties().putValue(PreviewProperty.NODE_OPACITY, 15);
+		model.getProperties().putValue(PreviewProperty.NODE_OPACITY, 10);
+
+		AttributeModel attributeModelLocal = Lookup.getDefault().lookup(AttributeController.class).getModel();
+		AttributeTable nodeTable = attributeModelLocal.getNodeTable();
+		AttributeColumn col;
+		col = nodeTable.getColumn(typeCol);
+		if (col == null) {
+			col = nodeTable.addColumn(typeCol, AttributeType.STRING);
+		}
+		PartitionController partitionController = Lookup.getDefault().lookup(PartitionController.class);
+		@SuppressWarnings("rawtypes")
+		Partition p = partitionController.buildPartition(col, gm.getGraph());
+		NodeColorTransformer nodeColorTransformer = new NodeColorTransformer();
+		nodeColorTransformer.randomizeColors(p);
+		partitionController.transform(p, nodeColorTransformer);
 
 		OpenOrdLayoutBuilder b = new OpenOrdLayoutBuilder();
 		Layout layout = b.buildLayout();
@@ -57,11 +82,23 @@ public class Lagravis {
 		}
 		layout.endAlgo();
 		ExportController ec = Lookup.getDefault().lookup(ExportController.class);
-		String outputFileName = inputFile.getName().replaceFirst("[.][^.]+$", "");
-		if (outputFileName.isEmpty()) outputFileName = "out";
-		outputFileName = outputFileName + ".pdf";
+		String outputName = inputFile.getName().replaceFirst("[.][^.]+$", "");
+		if (outputName.isEmpty()) outputName = "out";
+		String outputFileName;
+		File parent = inputFile.getAbsoluteFile().getParentFile();
+
+		// Export as PDF:
+		outputFileName = outputName + ".pdf";
 		try {
-			ec.exportFile(new File(inputFile.getAbsoluteFile().getParentFile(), outputFileName));
+			ec.exportFile(new File(parent, outputFileName));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		// Export as PNG:
+		outputFileName = outputName + ".png";
+		try {
+			ec.exportFile(new File(parent, outputFileName));
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
---
+++ src/main/resources/demo/graph2.gml
@@ -0,0 +1,87 @@
+graph [
+  directed 1
+  node [
+    id 0
+    type "A"
+  ]
+  node [
+    id 1
+    type "A"
+  ]
+  node [
+    id 2
+    type "A"
+  ]
+  node [
+    id 3
+    type "A"
+  ]
+  node [
+    id 4
+    type "A"
+  ]
+  node [
+    id 5
+    type "B"
+  ]
+  node [
+    id 6
+    type "B"
+  ]
+  node [
+    id 7
+    type "B"
+  ]
+  node [
+    id 8
+    type "B"
+  ]
+  node [
+    id 9
+    type "B"
+  ]
+  edge [
+    source 0
+    target 2
+  ]
+  edge [
+    source 1
+    target 2
+  ]
+  edge [
+    source 2
+    target 3
+  ]
+  edge [
+    source 3
+    target 4
+  ]
+  edge [
+    source 4
+    target 5
+  ]
+  edge [
+    source 6
+    target 3
+  ]
+  edge [
+    source 4
+    target 1
+  ]
+  edge [
+    source 2
+    target 5
+  ]
+  edge [
+    source 6
+    target 7
+  ]
+  edge [
+    source 7
+    target 8
+  ]
+  edge [
+    source 6
+    target 9
+  ]
+]
