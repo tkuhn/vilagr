dfeb4e30110a3f2c9916675464329f8ab8d80d8c
Tobias Kuhn <kuhntobias@gmail.com>
2014-07-15 09:25:34 +0200

    Add auxiliary script exec-class.sh

--- scripts/Vilagr.sh
+++ scripts/Vilagr.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-DIR=`pwd`
-
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd ..
-
-export MAVEN_OPTS="-Xmx8g"
-mvn -q -e exec:java -Duser.dir=$DIR -Dexec.mainClass="ch.tkuhn.vilagr.Vilagr" -Dexec.args="$*"
+$( dirname "${BASH_SOURCE[0]}" )/exec-class.sh ch.tkuhn.vilagr.Vilagr "$@"
